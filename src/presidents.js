95a4c45e3eed4c5d1c5cda080dcdc4bc5b3be8e8
José Baldino <josemanuelbaldino@gmail.com>
2025-04-08 14:47:57 +0100

    first commit

--- src/presidents.js
+++ src/presidents.js
@@ -419,27 +419,43 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map ((presidents) => presidents.name)
+}
 
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
-
-
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter ((presidents) => presidents.party === "Democratic")
+}
 
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
 
+  function  countYearsInOffice(presidentsArr) {
+    
+    const officeYears = presidentsArr.reduce (function (acc , president){
+      if (president.leftOffice === null) {
+        return acc
+      } else {
+      return acc + (president.leftOffice - president.tookOffice)
+      }
+    },0)
 
+    return officeYears
+  }
 
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
 
+function sortPresidentsByBirthYear(presidentsArr) {
 
+    const presidentsAge = presidentsArr.sort ((presidentA , presidentB) => {
+      return presidentA.birthYear - presidentB.birthYear
+    })
+
+    return presidentsAge
+}
 
 
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
